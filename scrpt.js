a4d40186026a119bb7bc25ff5dad5b6d60073ff7
fatima rahimi <112756954+iamfatimarahimi@users.noreply.github.com>
2024-08-26 09:48:16 +0330

    removing extra comments

--- scrpt.js
+++ scrpt.js
@@ -16,16 +16,6 @@ jq(function () {
   selectAll.change(function () {
     updateCheckboxes();
   });
-  // Select All button
-  // selectAll.change(function () {
-  //   if (jq(this).prop("checked")) {
-  //     singleCheck.prop("checked", true);
-  //     jq(".slc-all-label").text("Remove All");
-  //   } else {
-  //     singleCheck.prop("checked", false);
-  //     jq(".slc-all-label").text("Select All");
-  //   }
-  // });
   // single checkBoxes
   jq(document).on("change", ".single-slc", function () {
     let totalCheckboxes = jq(".single-slc").length;
@@ -36,29 +26,15 @@ jq(function () {
       jq(".slc-all-label").text("Remove All");
   });
 
-  // Adding the new row
+// Adding a new row
   jq(".add").click(function () {
     let textInput = jq(".textAdd").val();
     let tableRow = jq(".table tr").eq(1).clone();
     tableRow.find("input:checked").prop("checked", false);
-    tableRow.find(".name").text(textInput);
-
-    // First Way
-    // const lastTableRowNumber =
-    //   Number(
-    //     jq(".table tr")
-    //       .eq(jq(".table tr").length - 1)
-    //       .find(".number")
-    //       .text()
-    //   ) + 1;
-    // tableRow.find(".number").text(lastTableRowNumber);
-
-    // Second Way
+    tableRow.find(".name").text(textInput).find(".number").text(lastTableRowNumber);
+    // Dynamic Numbers
     const tableRowLength = jq(".table tr").length;
     tableRow.find(".number").text(tableRowLength);
     tableRow.appendTo(".table");
-    // if (selectAll.prop("checked")) {
-    //   tableRow.find("input").prop("checked", true);
-    // }
   });
 });
